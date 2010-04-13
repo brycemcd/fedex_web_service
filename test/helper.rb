9b758a1072936e36faf420ff8e7b50625e29cb69
bryce mcdonnell <bryce@bridgetownint.com>
2010-04-13 11:41:21 -0700

    Initial commit to fedex_web_service.

---
+++ test/helper.rb
@@ -0,0 +1,10 @@
+require 'rubygems'
+require 'test/unit'
+require 'shoulda'
+
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+require 'fedex_web_service'
+
+class Test::Unit::TestCase
+end
